bc74fa79cceaa17750af5bd44bb4becddf60df7e
Tobias Schmidt <t.schmidt@rubidat.de>
2012-12-07 18:23:13 +0100

    deleted some files

--- app/models/role.rb
+++
@@ -1,6 +0,0 @@
-class Role < ActiveRecord::Base
-  attr_accessible :comment, :name, :range
-
-  scope :membership, where(:range => 'membership')
-  scope :organisation, where(:range => 'organisation')
-end
